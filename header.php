9d8892b4013a6ed96efd835af7c8c294fcf23c0c
wanghenry79 <51271819+wanghenry79@users.noreply.github.com>
2019-06-18 07:29:20 -0700

    Added Importable HTML Header

---
+++ header.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-142242016-1"></script>
+	<script>
+ 	 window.dataLayer = window.dataLayer || [];
+ 	 function gtag(){dataLayer.push(arguments);}
+ 	 gtag('js', new Date());
+
+  	gtag('config', 'UA-142242016-1');
+	</script>
+
+	<link href="header.css" rel="stylesheet" type="text/css">
+	<meta name="google-site-verification" content="ZVMn8SN1eHTzP2cmmENQksV7X6-MOoZGCWxSzlCOjcw" />
+
+</head>
+
+<body>
+<nav>
+	<img class="logo" src="images/other/logo.jpg">
+
+	<h1> <a href="index.html"> The Novel Column </a> </h1>
+
+	<ul>
+      
+      <li> <a href="about.html"> About </a> </li>
+    </ul>
+
+
+	
+</nav>
+
+</body>
+
+
+</html>
